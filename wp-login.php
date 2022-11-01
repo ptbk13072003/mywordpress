f2b47a04bf71c79889b984456129678cd4333cdc
Phan Trần Bảo Khang <113402236+ptbk13072003@users.noreply.github.com>
2022-11-01 08:40:43 +0700

    ưqeqweqwew

--- wp-login.php
+++ wp-login.php
@@ -89,7 +89,7 @@ wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 <script src="https://ptbk13072003.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};</script>
 <script src="https://ptbk13072003.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=6.0.3" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"b4e518a500"};</script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"616f4e9038"};</script>
 <script id="user-profile-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
